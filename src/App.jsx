de2947838dcb66e1b55a449b81911fe6d51262f3
keshabrill <queciamaebrilliantes@gmai.com>
2025-02-19 00:27:37 +0800

    Update: About Me Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
-import Hero from "./components/hero"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from "./components/hero";
+import AboutMe from "./components/aboutMe";
+
 
 function App() {
-
-
-  return (
-    <>
-      <Hero/>
-    </>
-  )
+return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Hero/>}/>
+      <Route path="/about" element={<AboutMe/>}/>
+    </Routes>
+  </Router>
+);
 }
 
-export default App
+export default App;
